6f7ab7ab12961d0c61516e5f79976bb992351f8e
Paloma Cardozo <pmc8@gatech.edu>
2024-11-14 19:40:54 -0500

    Added documentation for 2 classes.

--- authentication/models.py
+++ authentication/models.py
@@ -4,6 +4,22 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
+    """
+        A model that stores additional information for a user, including their security question and answer.
+
+        This model is associated with a specific `User` through a one-to-one relationship and stores
+        a security question and its corresponding answer. It is used to manage user profiles that can
+        include personalized security information.
+
+        Attributes:
+            user (OneToOneField): A one-to-one relationship with the `User` model. This field links
+                                   each profile to a specific user.
+            security_question (CharField): A field that stores the security question associated with the user.
+            security_answer (CharField): A field that stores the answer to the security question.
+
+        Methods:
+            __str__(self): Returns the string representation of the profile, which is the username of the associated user.
+        """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     security_question = models.CharField(max_length=255)
     security_answer = models.CharField(max_length=255)
