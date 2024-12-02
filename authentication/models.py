4a2854ba4a3347e68a87c394330a9a19fa4f8a64
anushak016 <156885478+anushak016@users.noreply.github.com>
2024-12-01 23:49:13 -0500

    Update models.py

--- authentication/models.py
+++ authentication/models.py
@@ -33,12 +33,12 @@ class Profile(models.Model):
         """
         return self.user.username
 
-class TopSong(models.Model):
-        user = models.ForeignKey(User, on_delete=models.CASCADE)
-        title = models.CharField(max_length=255)
-        artist = models.CharField(max_length=255)
-        spotify_order = models.IntegerField(default=0)  # Add a default value
-        snippet_url = models.URLField(null=True, blank=True)
+# class TopSong(models.Model):
+#         user = models.ForeignKey(User, on_delete=models.CASCADE)
+#         title = models.CharField(max_length=255)
+#         artist = models.CharField(max_length=255)
+#         spotify_order = models.IntegerField(default=0)  # Add a default value
+#         snippet_url = models.URLField(null=True, blank=True)
 
-        def __str__(self):
-            return f"{self.title} by {self.artist}"
+#         def __str__(self):
+#             return f"{self.title} by {self.artist}"
